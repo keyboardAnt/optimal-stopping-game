55af170b4bd592b260a3b2c70290ac13cb4332a4
keyboardAnt <nadav0012@gmail.com>
2020-04-08 23:46:39 +0300

    add PlayerStopRandom, PlayerStopWeakAdvantage

--- src/classes/players.py
+++ src/classes/players.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod, abstractproperty
 from src.classes.cards import _Card, CardSimple
 from src.classes.flags import _Flag, FlagSimple
 from src import settings
+import numpy as np
 
 
 class _Player(ABC):
@@ -16,10 +17,7 @@ class _Player(ABC):
 class PlayerStopFirst(_Player):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
-        # assert '_num_of_red_cards' in kwargs
-        # self._num_of_cards_remain: int = 2 * self._num_of_red_cards
-        # self._num_of_red_cards_remain: int = self._num_of_red_cards
-        self._num_of_cards_remain: int = 2 * settings.Deck.NUM_OF_RED_CARDS
+        # self._num_of_total_cards_remain: int = 2 * settings.Deck.NUM_OF_RED_CARDS
         self._num_of_red_cards_remain: int = settings.Deck.NUM_OF_RED_CARDS
 
     def play(self, card: CardSimple) -> FlagSimple:
@@ -29,29 +27,53 @@ class PlayerStopFirst(_Player):
 class PlayerStopLast(_Player):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
-        # assert '_num_of_red_cards' in kwargs
-        # self._num_of_cards_remain: int = 2 * set._num_of_red_cards
-        # self._num_of_red_cards_remain: int = self._num_of_red_cards
-        self._num_of_cards_remain: int = 2 * settings.Deck.NUM_OF_RED_CARDS
-        self._num_of_red_cards_remain: int = settings.Deck.NUM_OF_RED_CARDS
+        self._num_of_total_cards_remain: int = 2 * settings.Deck.NUM_OF_RED_CARDS
+        # self._num_of_red_cards_remain: int = settings.Deck.NUM_OF_RED_CARDS
 
     def play(self, card: CardSimple) -> FlagSimple:
-        self._num_of_cards_remain -= 1
-        while self._num_of_cards_remain > 0:
+        self._num_of_total_cards_remain -= 1
+        while self._num_of_total_cards_remain > 0:
             return False
         return True
 
 
-# class PlayerSmart(_Player):
-    # def play(self, card: CardSimple) -> FlagSimple:
-    #     self._num_of_cards_remain -= 1
-    #     if card is True:
-    #         self._num_of_red_cards_remain -= 1
+class PlayerStopRandom(_Player):
+    """
+    Pick a random index in [0, # of cards in the deck - 1], and stop then.
+    """
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        num_of_cards: int = 2 * settings.Deck.NUM_OF_RED_CARDS
+        self._idx: int = 0
+        self._random_idx: int = np.random.randint(num_of_cards)
+
+    def play(self, card: CardSimple) -> FlagSimple:
+        if self._idx == self._random_idx:
+            return True
+        self._idx += 1
+        return False
 
 
+class PlayerStopWeakAdvantage(_Player):
+    """
+    Stop immediately when detecting a small advantage
+    """
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self._num_of_total_cards_remain: int = 2 * settings.Deck.NUM_OF_RED_CARDS
+        self._num_of_red_cards_remain: int = settings.Deck.NUM_OF_RED_CARDS
+        self.WEAK_ADVANTAGE = .55
+
+    def play(self, card: CardSimple) -> FlagSimple:
+        self._num_of_total_cards_remain -= 1
+        if card is True:
+            self._num_of_red_cards_remain -= 1
+        if self._num_of_red_cards_remain / self._num_of_total_cards_remain > self.WEAK_ADVANTAGE:
+            return True
+        return False
 
     # def _is_at_least_half_of_remain_cards_are_red(self) -> bool:
-    #     return self._num_of_red_cards_remain / self._num_of_cards_remain >= .5
+    #     return self._num_of_red_cards_remain / self._num_of_total_cards_remain >= .5
 
 
 
--- src/settings.py
+++ src/settings.py
@@ -1,26 +1,23 @@
 from typing import Final, List, Type
-from src.classes.players import _Player, PlayerStopFirst, PlayerStopLast
+from src.classes.players import _Player, PlayerStopFirst, PlayerStopLast, PlayerStopRandom, PlayerStopWeakAdvantage
 from src.classes.decks import _Deck, DeckSimple
 from os.path import join
 
 
 class Deck:
     CLASS: [_Deck] = DeckSimple
-    NUM_OF_RED_CARDS: Final[int] = 1000
+    NUM_OF_RED_CARDS: Final[int] = 100_000
 
 class Players:
     PLAYERS_TO_EVALUATE: List[Type[_Player]] = [
-        # PlayerStopFirst(_num_of_red_cards=Deck.NUM_OF_RED_CARDS),
         PlayerStopFirst,
         PlayerStopLast,
+        PlayerStopRandom,
+        PlayerStopWeakAdvantage,
     ]
 
 
 class Evaluate:
-    # PLAYERS_TO_EVALUATE: List[Type[_Player]] = [
-    #     # PlayerStopFirst(_num_of_red_cards=Deck.NUM_OF_RED_CARDS),
-    #     PlayerStopLast,
-    # ]
     NUM_OF_GAMES: int = 1_000
 
 
